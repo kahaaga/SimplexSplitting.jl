4b05bdd31cc45cb22c21962cbfc5fc456c95883f
kahaaga <kahaaga@gmail.com>
2017-12-04 11:10:27 +0100

    Added simplicial subdivision routine.

--- src/SimplexSplitting.jl
+++ src/SimplexSplitting.jl
@@ -1,6 +1,11 @@
 module SimplexSplitting
 
 include("tensordecomposition.jl")
+include("complementary.jl")
+include("simplex_split.jl")
+include("simplex_subdivision.jl")
+include("heaviside.jl")
 
-
+export tensordecomposition, simplex_split, simplicial_subdivision_single,
+    simplicial_subdivision
 end # module
