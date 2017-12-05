ea0235059ed2f5a6a2bc8213074f7075335b31bf
kahaaga <kahaaga@gmail.com>
2017-12-05 23:04:15 +0100

    Add more tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using SimplexSplitting
 using Base.Test
 
-# write your own tests here
 include("test_tensordecomposition.jl")
 include("test_embed.jl")
 include("test_triangulate.jl")
+include("test_refine_triangulation.jl")
