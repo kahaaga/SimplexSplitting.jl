bc18f0eab313d00c1a4f2371bb92d9f819088ab8
kahaaga <kahaaga@gmail.com>
2017-12-05 23:04:00 +0100

    Test embedding

--- test/test_embed.jl
+++ test/test_embed.jl
@@ -1,3 +1,6 @@
-ex1 = SimplexSplitting.embedding_example(10, 3, 1)
-@test size(ex1)[1] == 8
-@test size(ex1)[2] == 3
+
+@testset "Embedding" begin
+    ex1 = embedding_example(10, 3, 1)
+    @test size(ex1)[1] == 8
+    @test size(ex1)[2] == 3
+end
