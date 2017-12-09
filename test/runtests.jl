93d5df98dd01570af77c4636e162c01aa0f255c7
kahaaga <kahaaga@gmail.com>
2017-12-09 18:27:45 +0100

    Added tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using SimplexSplitting
 using Base.Test
 
-#include("test_tensordecomposition.jl")
-#include("test_embed.jl")
-#include("test_triangulate.jl")
-#include("test_refine_triangulation.jl")
-#include("test_refine_recursive.jl")
+include("test_tensordecomposition.jl")
+include("test_embed.jl")
+include("test_triangulate.jl")
+include("test_refine_triangulation.jl")
+include("test_refine_recursive.jl")
 include("test_refine_recursive_with_images.jl")
