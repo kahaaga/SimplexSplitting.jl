fb7ab0064e876b9623c4fe70f3867cac8eb0bcd6
kahaaga <kahaaga@gmail.com>
2018-01-17 12:05:06 +0100

    Fixed tests for refinement with images.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
 using SimplexSplitting
 using Base.Test
 
+# Only run these if the refinement tests fail
 #include("test_tensordecomposition.jl")
 #include("test_embed.jl")
 #include("test_triangulate.jl")
-#include("test_refine_triangulation.jl")
+
+# If these succeed, then the above doesn't need testing
+include("test_refine_triangulation.jl")
+include("test_refine_triangulation_with_images.jl")
 include("test_refine_recursive.jl")
 include("test_refine_recursive_with_images.jl")
--- test/test_refine_triangulation_with_images.jl
+++ test/test_refine_triangulation_with_images.jl
@@ -1,11 +1,13 @@
-n_points = 10
-tau = 1
+
 
 @testset "Refine triangulation with images" begin
     @testset "E = $E" for E in 3:5
-        @testset "k = $k" for k in 1:4
+        @testset "k = $k" for k in 1:3
+            n_points = 10
+            tau = 1
+            #println(E, " ", k)
             splitting_rules = simplicial_subdivision(k, E)
-            embedding = embedding_example(n_points, E, 1)
+            embedding = embedding_example(n_points+E*2, E, tau)
 
             # Triangulate all but the last point
             triang_points, simplex_indices = triangulate(embedding[1:size(embedding, 1), :])
@@ -13,23 +15,26 @@ tau = 1
             # Take the images of the simplices in the triangulation as the forward projections
             # in time of the original simplices.
             image_indices = simplex_indices + 1
-            image_vertices = vcat(triang_points[2:end, :], embedding[1, :].')
-            @show E, k, triang_points, simplex_indices, image_vertices
-            println()
+            image_points = vcat(triang_points[2:end, :], embedding[1, :].')
+            #@show E, k, triang_points, simplex_indices, image_points
+
             # How many simplices did we get?
             n_orig = size(simplex_indices, 1)
 
             simplices = [triang_points[simplex_indices[i, :], :] for i in 1:n_orig]
-            refined_triangulation = refine_triangulation_images(triang_points, simplex_indices,
-                                    splitting_rules, image_vertices, collect(1:3), k)
+
+            split_indices = collect(1:size(embedding, 2))
+
+            refined_triang = refine_triangulation_images(
+                triang_points, simplex_indices, image_points, split_indices, k)
 
             # How many new simplices did we get?
-            n_refined = size(refined_triangulation[2], 1)
-            refined_simplices = [refined_triangulation[1][refined_triangulation[2][i, :], :]
+            n_refined = size(refined_triang[2], 1)
+            refined_simplices = [refined_triang[1][refined_triang[2][i, :], :]
                                     for i in 1:n_refined]
 
-            refined_vertices = refined_triangulation[1]
-            refined_simplex_indices = refined_triangulation[2]
+            refined_vertices = refined_triang[1]
+            refined_simplex_indices = refined_triang[2]
 
             @testset "Arrays are of right dimensions and types" begin
                 @test isa(refined_vertices, Array{Float64, 2})
